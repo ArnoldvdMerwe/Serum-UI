da52e54f7be88ba1017977ff49f0fa3e1168dff8
Arnold van der Merwe <arnold1.vandermerwe@gmail.com>
2023-01-23 20:56:30 +0200

    src/pages/CandlestickPage: Gets data from back end
    The front end now requests the back end for the candlestick data.
    The selection of time division for minute, half-hour, hour and
    day was implemented.
    
    Signed-off-by: Arnold van der Merwe <arnold1.vandermerwe@gmail.com>

--- src/pages/CandlestickPage.vue
+++ src/pages/CandlestickPage.vue
@@ -1,26 +1,13 @@
 <script lang="ts">
+import axios from 'axios';
 import {
   CandlestickData,
   ChartOptions,
   createChart,
   IChartApi,
   ISeriesApi,
-  Time
 } from 'lightweight-charts';
 import { defineComponent } from 'vue';
-
-const data: CandlestickData[] = [
-  { open: 10, high: 10.63, low: 9.49, close: 9.55, time: 1642427876 as Time },
-  { open: 9.55, high: 10.3, low: 9.42, close: 9.94, time: 1642514276 as Time },
-  { open: 9.94, high: 10.17, low: 9.92, close: 9.78, time: 1642600676 as Time },
-  { open: 9.78, high: 10.59, low: 9.18, close: 9.51, time: 1642687076 as Time },
-  { open: 9.51, high: 10.46, low: 9.1, close: 10.17, time: 1642773476 as Time },
-  { open: 10.17, high: 10.96, low: 10.16, close: 10.47, time: 1642859876 as Time},
-  { open: 10.47, high: 11.39, low: 10.4, close: 10.81, time: 1642946276 as Time},
-  { open: 10.81, high: 11.6, low: 10.3, close: 10.75, time: 1643032676 as Time},
-  { open: 10.75, high: 11.6, low: 10.49, close: 10.93, time: 1643119076 as Time},
-  { open: 10.93, high: 11.53, low: 10.76, close: 10.96, time: 1643205476 as Time}
-];
 
 export default defineComponent({
   name: 'CandlestickPage',
@@ -29,11 +16,13 @@ export default defineComponent({
   data() {
     return {
       chart: {} as IChartApi,
-      candlestickSeries: {} as ISeriesApi<'Candlestick'>
+      candlestickSeries: {} as ISeriesApi<'Candlestick'>,
+      timeDivision: 'Minute',
+      timeDivisionList: ['Minute', 'Half-hour', 'Hour', 'Day']
     };
   },
 
-  mounted() {
+  async mounted() {
     const chartOptions = {
       layout: {
         textColor: 'white',
@@ -54,12 +43,16 @@ export default defineComponent({
         horzLine: {
           color: 'white'
         }
+      },
+      timeScale: {
+            timeVisible: true
       }
     };
     this.chart = createChart(
       this.$refs.chartContainer as HTMLElement,
       chartOptions as ChartOptions
     );
+
     this.candlestickSeries = this.chart.addCandlestickSeries({
       upColor: 'rgba(0, 255, 0, 1)',
       downColor: 'rgba(255, 0, 0, 1)',
@@ -67,10 +60,45 @@ export default defineComponent({
       wickUpColor: 'rgba(0, 255, 0, 0.5)',
       wickDownColor: 'rgba(255, 0, 0, 0.5)'
     });
-
-    this.candlestickSeries.setData(data);
-
+    this.candlestickSeries.setData(await this.fetchCandlestickData());
     this.chart.timeScale().fitContent();
+  },
+
+  methods: {
+    async fetchCandlestickData(): Promise<CandlestickData[]> {
+      let string = '';
+      switch (this.timeDivision) {
+        case 'Minute':
+          string = 'minute';
+          break;
+        case 'Half-hour':
+          string = 'halfhour';
+          break;
+        case 'Hour':
+          string = 'hour';
+          break;
+        case 'Day':
+          string = 'day';
+      }
+      const response = await axios.get(
+        `http://localhost:5000/api/candlestickdata/transactions/${string}`
+      );
+      return response.data as CandlestickData[];
+    },
+
+    async getNewCandlestickData() {
+      this.chart.removeSeries(this.candlestickSeries);
+
+      this.candlestickSeries = this.chart.addCandlestickSeries({
+        upColor: 'rgba(0, 255, 0, 1)',
+        downColor: 'rgba(255, 0, 0, 1)',
+        borderVisible: false,
+        wickUpColor: 'rgba(0, 255, 0, 0.5)',
+        wickDownColor: 'rgba(255, 0, 0, 0.5)'
+      });
+      this.candlestickSeries.setData(await this.fetchCandlestickData());
+      this.chart.timeScale().fitContent();
+    }
   }
 });
 </script>
@@ -90,8 +118,17 @@ q-page(
   h6.row.flex-center.bg-black.text-white(
         style = 'margin: 0px;'
     ) Time
-  q-img(
-    src = 'https://www.wikihow.com/images/thumb/e/eb/Read-a-Candlestick-Chart-Step-1.jpg/v4-460px-Read-a-Candlestick-Chart-Step-1.jpg.webp'
-    style="max-height: 300px; max-width: 550px;"
-  )
+  div.row.justify-around
+    q-img(
+      src = 'https://www.wikihow.com/images/thumb/e/eb/Read-a-Candlestick-Chart-Step-1.jpg/v4-460px-Read-a-Candlestick-Chart-Step-1.jpg.webp'
+      style="max-height: 300px; max-width: 550px;"
+    )
+    q-select.col-2(
+      bg-color = "white"
+      filled
+      v-model = 'timeDivision'
+      :options = 'timeDivisionList'
+      label = 'Time division'
+      @update:model-value = 'getNewCandlestickData();'
+    )
 </template>
